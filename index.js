d6a7000b3ec2cfaff797b8dc3176ac5b0c3896b4
Taylor Olson <pipestay622@gmail.com>
2021-02-03 22:01:37 -0800

    Task 2 complete

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ The function should:
   Example createMenuItem('tacos', 8, 'Lunch') should return {name: 'tacos', price: 8, category: 'Lunch'}
 */
 
-function createMenuItem(/*Your code here*/){
-    /*Your code here*/
-}
+function createMenuItem(name, price, category){
+  return {name, price, category}
+};
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1b: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Invoke your function!
@@ -28,6 +28,10 @@ Test your createMenuItems function by doing the following:
   
   For example: createMenuItem("pizza",5,"lunch") would return this as the object: {name:"Pizza",price:5,category:"lunch"}
 */
+
+console.log(createMenuItem("Wisconsin Mac", 12, "Lunch"));
+console.log(createMenuItem("Pancakes", 5, "Breakfast"));
+console.log(createMenuItem("Meatloaf", 7, "Dinner"));
 
 
 
@@ -48,8 +52,14 @@ export const burger = {
   name: "Burger", 
   price: 18, 
   category: "Lunch", 
-  /*Your code here*/
-}
+  discount: function(customerStatus) {
+    if (customerStatus === "teacher" || "student") {
+      return this.price*0.75
+  } else {
+    return this.price*0.90
+  }
+  },
+};
 
 
 
